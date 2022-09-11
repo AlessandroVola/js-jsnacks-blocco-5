6cbf1457d70ad3ecc42f707042f551744983c479
AlessandroVola <volaalessandro@gmail.com>
2022-09-12 00:27:31 +0200

    info snake 2 e 3 added

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 console.log(`JS OK!`);
+
 /* SNACK 1*/
 /*
  * Creare un array di oggetti di squadre di calcio. 
@@ -11,6 +12,7 @@ console.log(`JS OK!`);
 // Creare un array di oggetti di squadre di calcio.
 // Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
 // Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+console.log(`JS SNACK 1`);
 const footballTeams = [
     {
         name: `Milan`,
@@ -51,8 +53,32 @@ for (i = 0; i < footballTeams.length; i++) {
 
 console.log(footballTeams);
 
+
+
 function getRandomNumber(min, max) {
     const range = max - min + 1;
     const random = Math.floor(Math.random() * range) + min;
     return random
 }
+
+
+/* SNACK 2*/
+/*
+ * Si scriva una funzione che accetti tre argomenti, 
+ * un array e due numeri (a più piccolo di b). 
+ * La funzione ritornerà un nuovo array con i valori 
+ * che hanno la posizione compresa tra i due numeri.
+ */
+
+console.log(`JS SNACK 2`)
+function threeVariables(array, a, b) {
+    const array = [];
+    number = 0
+}
+
+/* SNACK 3*/
+/* 
+ * Crea 10 oggetti che rappresentano una zucchina.
+ * Dividi in due array separati le zucchine che misurano meno o più di 15cm.
+ * Infine stampa separatamente quanto pesano i due gruppi di zucchine.
+ */
